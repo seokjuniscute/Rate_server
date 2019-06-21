d05aac6891e657daf382d9548244caba1bb754b3
yuicraft <twins20131988@gmail.com>
2019-06-21 20:35:00 +0900

    spell change

--- app/controllers/reminder_controller.rb
+++ app/controllers/reminder_controller.rb
@@ -3,8 +3,6 @@ class ReminderController < ApplicationController
         if(User.exists?(ide: params[:id]))
             if(params[:content] == "")
                 head :bad_request
-            elsif(!(params[:hour].kind_of?(Fixnum) || params[:minutes].kind_of?(Fixnum) || params[:importance].kind_of?(Fixnum)))
-                head :bad_request
             elsif(params[:hours].to_i < 0 || params[:hours].to_i > 24 || params[:minutes].to_i < 0 || params[:minutes].to_i > 60 || params[:importance].to_i < 0 || params[:importance].to_i > 5)
                 head :bad_request
             elsif(params[:month].to_i>12 || params[:month].to_i < 0 || params[:day] > 31 || params[:day].to_i < 0 || params[:year].to_i < 0)
@@ -22,7 +20,7 @@ class ReminderController < ApplicationController
 
     def send_reminder
         if(User.exists?(ide: params[:id]))
-            if(params[:hour].kind_of?(Fixnum) && params[:minutes].kind_of?(Fixnum) && !(params[:hours].to_i < 0 || params[:hours].to_i > 24 || params[:minutes].to_i < 0 || params[:minutes].to_i > 60) )
+            if(!(params[:hours].to_i < 0 || params[:hours].to_i > 24 || params[:minutes].to_i < 0 || params[:minutes].to_i > 60) )
                 if(Reminder.exists?(ide: params[:id],year: params[:year],month: params[:month],hour: params[:hours],month: params[:month],day: params[:day]))
                     reminder = Reminder.find_by(ide: params[:id],year: params[:year],month: params[:month],hour: params[:hours],month: params[:month],day: params[:day])
                     render json: {
