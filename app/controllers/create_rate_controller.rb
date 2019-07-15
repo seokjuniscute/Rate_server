b201c9f39a37fdc3ce6fdb83b8b4b68a7830c187
yuicraft <twins20131988@gmail.com>
2019-07-15 16:14:22 +0900

    Spell Checked

--- app/controllers/create_rate_controller.rb
+++ app/controllers/create_rate_controller.rb
@@ -6,7 +6,7 @@ class CreateRateController < ApplicationController
             elsif(params[:month].to_i>12 || params[:month].to_i < 0 || params[:day].to_i > 31 || params[:day].to_i < 0 || params[:year].to_i < 0)
                 head :bad_request
             else
-                Rating.create(ide: params[:id], what_you_bad: params[:what_you_bad],what_you_do: params[:what_you_do],what_you_good: [:what_you_good],year: params[:year].to_i,month: params[:month].to_i,day: params[:month].to_i)
+                Rating.create(ide: params[:id], what_you_bad: params[:what_you_bad],what_you_do: params[:what_you_do],what_you_good: params[:what_you_good],year: params[:year].to_i,month: params[:month].to_i,day: params[:month].to_i)
                 head :ok
             end
         elsif(!(User.exists?(ide: params[:id])))
